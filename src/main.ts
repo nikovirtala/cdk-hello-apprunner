0007205ef027ba92bc89e759559e6aec00689a60
Niko Virtala <nikovirtala@users.noreply.github.com>
2024-11-09 11:00:16 +0200

    chore(deps): upgrade dependencies (#848)
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/nikovirtala/cdk-hello-apprunner/actions/runs/11751469955
    
    ------
    
    *Automatically created by projen via the "upgrade" workflow*

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,27 @@
-import * as apprunner from '@aws-cdk/aws-apprunner-alpha';
-import { App, CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as apprunner from "@aws-cdk/aws-apprunner-alpha";
+import { App, CfnOutput, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 export class AppRunnerStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    const service = new apprunner.Service(this, 'service', {
+    const service = new apprunner.Service(this, "service", {
       source: apprunner.Source.fromEcrPublic({
         imageConfiguration: { port: 8000 },
-        imageIdentifier: 'public.ecr.aws/aws-containers/hello-app-runner:latest',
+        imageIdentifier:
+          "public.ecr.aws/aws-containers/hello-app-runner:latest",
       }),
     });
 
-    new CfnOutput(this, 'url', {
-      value: 'https://' + service.serviceUrl,
+    new CfnOutput(this, "url", {
+      value: "https://" + service.serviceUrl,
     });
   }
 }
 
 const app = new App();
 
-new AppRunnerStack(app, 'hello-apprunner');
+new AppRunnerStack(app, "hello-apprunner");
 
 app.synth();
